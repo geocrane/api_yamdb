975e4f98cab75aff2fe6d02fc58403692f8858bb
«Сергей <sergei.eliseev93@yandex.ru>
2022-11-15 23:21:24 +0300

    Add_Django_administration_ver_0.03

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,19 @@
+""" Настройки админ панели."""
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Category, Genre, Review, Comment
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    """ Настройка отображения по отзывам в админ панели."""
+    list_display = ('pk', 'text', 'pub_date', 'author', 'title',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Review)
+admin.site.register(Comment)
