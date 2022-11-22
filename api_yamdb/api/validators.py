e914319acbb0ace27c3a5e6a4c1fb4a81f219fa8
Сергей Журавлев <geo-rnd@yandex.ru>
2022-11-22 20:01:51 +0300

    Изменил валидаторы под новую логику авторизации

--- api_yamdb/api/validators.py
+++ api_yamdb/api/validators.py
@@ -3,25 +3,28 @@ from rest_framework import serializers
 from reviews.models import User
 
 
-def validate_username(value):
-    if len(User.objects.filter(username=value)) != 0:
+def is_user_exist(value):
+    if User.objects.filter(username=value):
         raise serializers.ValidationError(
             "Пользователь с таким username уже зарегистрирован"
         )
-    if value == "me":
-        raise serializers.ValidationError("Имя 'me' недопустимо")
+
+
+def is_email_exist(value):
+    if User.objects.filter(email=value):
+        raise serializers.ValidationError(
+            "Пользователь с таким email уже зарегистрирован"
+        )
+    return value
+    
+
+def validate_username(value):
+    if not re.match("^[A-Za-z0-9@._-]*$", value):
+        raise serializers.ValidationError("Недопустимые символы в username")
     if len(value) > 150:
         raise serializers.ValidationError(
             "Длина username не может превышать 150 символов"
         )
-    if not re.match("^[A-Za-z0-9@._-]*$", value):
-        raise serializers.ValidationError("Недопустимые символы в username")
-    return value
-
-
-def validate_email(value):
-    if len(User.objects.filter(email=value)) != 0:
-        raise serializers.ValidationError(
-            "Пользователь с таким email уже зарегистрирован"
-        )
+    if value == "me":
+        raise serializers.ValidationError("Имя 'me' недопустимо")
     return value
