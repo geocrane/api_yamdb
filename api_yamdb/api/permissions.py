24bdab5ed183b3268b2168b21f8bb06fedbac64c
Сергей Журавлев <geo-rnd@yandex.ru>
2022-11-23 17:45:03 +0300

    Исправления permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,26 +5,14 @@ class AdminPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.auth:
             return False
-        if request.user.is_admin or request.user.is_staff:
-            return True
+        return request.user.is_admin
 
 
 class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if not request.auth:
-            return False
-        if request.user.is_admin or request.user.is_staff:
-            return True
-        return False
-
-
-class UserPermissions(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if not request.auth:
-            return False
-        return True
+        return (
+            request.auth and request.user.is_admin
+        ) or request.method in permissions.SAFE_METHODS
 
 
 class AuthorOrReviewerOrReadOnly(permissions.BasePermission):
@@ -39,5 +27,4 @@ class AuthorOrReviewerOrReadOnly(permissions.BasePermission):
             or obj.author == request.user
             or request.user.is_admin
             or request.user.is_moderator
-            or request.user.is_staff
         )
